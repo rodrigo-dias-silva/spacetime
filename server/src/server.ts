fa91f1cfe7d6a1566604122fe042b38cfd13234c
Rodrigo Linux <rodrigo-cleiton@hotmail.com>
2023-05-17 00:06:51 -0300

    feat: config de eslint e instalacao de dependencias

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,14 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/hello',()=>{
+app.get('/hello', () => {
   return 'Hello World'
 })
 
-app.listen({
-  port:3333
-}).then(()=>{
-  console.log(' 🌐 HTTP server running on http://localhost:3333');
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log(' 🌐 HTTP server running on http://localhost:3333')
+  })
