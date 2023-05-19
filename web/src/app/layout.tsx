a97aa04f3e696c22678b7691bd3e49db697254a1
Rodrigo Linux <rodrigo-cleiton@hotmail.com>
2023-05-19 00:01:04 -0300

    feat: config fonts do projeto

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -16,7 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans`}>
+        {children}
+      </body>
     </html>
   )
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="h-screen w-full bg-zinc-950">
-      <h1 className="text-2xl text-zinc-100">Hello World</h1>
+      <h1 className="text-4xl font-bold text-zinc-100">Hello World</h1>
+      <h1 className="font-alt text-4xl font-bold text-zinc-100">Hello World</h1>
     </div>
   )
 }
